b274876249d3bafefca33e8f68cbd2e18dff2825
faizadli <mochfaizadli@gmail.com>
2021-03-03 13:51:26 +0700

    Lesson 3 - Fetching Data from Firebase and Display it in HomeView

--- Food App/Food_AppApp.swift
+++ Food App/Food_AppApp.swift
@@ -6,12 +6,26 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct Food_AppApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
+}
+
+// initializing firebase
+class AppDelegate: NSObject, UIApplicationDelegate {
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions
+      launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    
+    FirebaseApp.configure()
+    return true
+  }
 }
