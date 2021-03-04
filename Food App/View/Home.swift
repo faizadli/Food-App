2b34c289dc2011e1a85d4b194b772aa08743ea78
faizadli <mochfaizadli@gmail.com>
2021-03-04 13:38:39 +0700

    Lesson 4 - Create CartView and Fixed Project

--- Food App/View/Home.swift
+++ Food App/View/Home.swift
@@ -43,96 +43,105 @@ struct Home: View {
             .foregroundColor(.gray)
           
           TextField("Search", text: $HomeModel.search)
-        
+          
         }
         .padding(.horizontal)
         .padding(.top, 10)
         
         Divider()
         
-        ScrollView(.vertical, showsIndicators: false, content: {
-          VStack(spacing: 25){
-            ForEach(HomeModel.filtered){ item in
-              
-              ZStack(alignment: Alignment(horizontal: .center, vertical: .top), content: {
-                
-                ItemView(item: item)
-                
-                HStack{
-                  Text("FREE DELIVERY")
-                    .foregroundColor(.white)
-                    .padding(.vertical, 10)
-                    .padding(.horizontal)
-                    .background(Color.pink)
-                  
-                  Spacer(minLength: 0)
-                  
-                  Button(action: {}, label: {
-                    Image(systemName: "plus")
-                      .foregroundColor(.white)
-                      .padding(10)
-                      .background(Color.pink)
-                      .clipShape(Circle())
-                  })
-                }
-                .padding(.trailing, 10)
-                .padding(.top, 10)
-                
-              })
-              .frame(width: UIScreen.main.bounds.width - 30)
-            }
-          }
-          .padding(.top, 10)
-        })
-        
-      }
-      
-      HStack{
-        Menu(homeData: HomeModel)
-          .offset(x: HomeModel.showMenu ? 0 : -UIScreen.main.bounds.width / 1.6)
-        
-        Spacer(minLength: 0)
-      }
-      .background(Color.black.opacity(HomeModel.showMenu ? 0.3 : 0).ignoresSafeArea()
-                    .onTapGesture(perform: {
-                      withAnimation(.easeIn){HomeModel.showMenu.toggle()}
-                    })
-      )
-      if HomeModel.noLocation {
-        Text("Please Enable Location Access In Settiings To Further Move On !!")
-          .foregroundColor(.black)
-          .frame(width: UIScreen.main.bounds.width - 100, height: 120)
-          .background(Color.white)
-          .cornerRadius(10)
-          .frame(maxWidth: .infinity, maxHeight: .infinity)
-          .background(Color.black.opacity(0.3).ignoresSafeArea())
-      }
-      
-    }
-    .onAppear(perform: {
-      HomeModel.locationManager.delegate = HomeModel
-    })
-    .onChange(of: HomeModel.search, perform: { value in
-      
-      // to avoid continues search request
-      DispatchQueue.main.asyncAfter(deadline: .now() + 0.3){
-        
-        if value == HomeModel.search && HomeModel.search != ""{
-          // search data
-          HomeModel.filterData()
+        if HomeModel.items.isEmpty{
+          Spacer()
+          ProgressView()
+          Spacer()
         }
+        else{
+          ScrollView(.vertical, showsIndicators: false, content: {
+            VStack(spacing: 25){
+              ForEach(HomeModel.filtered){ item in
+                
+                ZStack(alignment: Alignment(horizontal: .center, vertical: .top), content: {
+                  
+                  ItemView(item: item)
+                  
+                  HStack{
+                    Text("FREE DELIVERY")
+                      .foregroundColor(.white)
+                      .padding(.vertical, 10)
+                      .padding(.horizontal)
+                      .background(Color.pink)
+                    
+                    Spacer(minLength: 0)
+                    
+                    Button(action: {
+                      HomeModel.addToCart(item: item)
+                    }, label: {
+                      Image(systemName: item.isAdded ? "checkmark" : "plus")
+                        .foregroundColor(.white)
+                        .padding(10)
+                        .background(item.isAdded ? Color.green :Color.pink)
+                        .clipShape(Circle())
+                    })
+                  }
+                  .padding(.trailing, 10)
+                  .padding(.top, 10)
+                  
+                })
+                .frame(width: UIScreen.main.bounds.width - 30)
+              }
+            }
+            .padding(.top, 10)
+          })
+        }
+        
       }
-      
-      if HomeModel.search == ""{
-        // reset all data
-        withAnimation(.linear){ HomeModel.filtered = HomeModel.items}
+        
+        HStack{
+          Menu(homeData: HomeModel)
+            .offset(x: HomeModel.showMenu ? 0 : -UIScreen.main.bounds.width / 1.6)
+          
+          Spacer(minLength: 0)
+        }
+        .background(Color.black.opacity(HomeModel.showMenu ? 0.3 : 0).ignoresSafeArea()
+                      .onTapGesture(perform: {
+                        withAnimation(.easeIn){HomeModel.showMenu.toggle()}
+                      })
+        )
+        if HomeModel.noLocation {
+          Text("Please Enable Location Access In Settiings To Further Move On !!")
+            .foregroundColor(.black)
+            .frame(width: UIScreen.main.bounds.width - 100, height: 120)
+            .background(Color.white)
+            .cornerRadius(10)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.black.opacity(0.3).ignoresSafeArea())
+        }
+        
       }
-    })
+      .onAppear(perform: {
+        HomeModel.locationManager.delegate = HomeModel
+      })
+      .onChange(of: HomeModel.search, perform: { value in
+        
+        // to avoid continues search request
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3){
+          
+          if value == HomeModel.search && HomeModel.search != ""{
+            // search data
+            HomeModel.filterData()
+          }
+        }
+        
+        if HomeModel.search == ""{
+          // reset all data
+          withAnimation(.linear){ HomeModel.filtered = HomeModel.items}
+        }
+      })
+    }
   }
-}
-
-struct Home_Previews: PreviewProvider {
-  static var previews: some View {
-    Home()
+  
+  struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+      Home()
+    }
   }
-}
--- Food App/ViewModel/HomeViewModel.swift
+++ Food App/ViewModel/HomeViewModel.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 import CoreLocation
 import Firebase
+import FirebaseAuth
+import FirebaseFirestore
 
 class HomeViewModel: NSObject,ObservableObject,CLLocationManagerDelegate {
   
@@ -20,8 +22,12 @@ class HomeViewModel: NSObject,ObservableObject,CLLocationManagerDelegate {
   
   @Published var showMenu = false
   
+  // item data
   @Published var items: [Item] = []
   @Published var filtered: [Item] = []
+  
+  @Published var cartItems: [Cart] = []
+  @Published var ordered = false
   
   func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
     switch manager.authorizationStatus {
@@ -51,13 +57,13 @@ class HomeViewModel: NSObject,ObservableObject,CLLocationManagerDelegate {
   func extractLocation() {
     CLGeocoder().reverseGeocodeLocation(self.userLocation) { (res, err) in
       guard let safeData = res else {return}
-    
+      
       var address = ""
-    
+      
       address += safeData.first?.name ?? ""
       address += ", "
       address += safeData.first?.locality ?? ""
-    
+      
       self.userAddress = address
     }
   }
@@ -76,43 +82,138 @@ class HomeViewModel: NSObject,ObservableObject,CLLocationManagerDelegate {
       self.fetchData()
     }
   }
+  
+  func fetchData(){
     
-    func fetchData(){
+    let db = Firestore.firestore()
+    
+    db.collection("Items").getDocuments { (snap, err) in
       
-      let db = Firestore.firestore()
+      guard let itemData = snap else { return }
       
-      db.collection("Items").getDocuments { (snap, err) in
+      self.items = itemData.documents.compactMap({ (doc) -> Item? in
         
-        guard let itemData = snap else { return }
+        let id = doc.documentID
+        let name = doc.get("item_name") as! String
+        let cost = doc.get("item_cost") as! NSNumber
+        let ratings = doc.get("item_ratings") as! String
+        let image = doc.get("item_image") as! String
+        let details = doc.get("item_details") as! String
         
-        self.items = itemData.documents.compactMap({ (doc) -> Item? in
-          
-          let id = doc.documentID
-          let name = doc.get("item_name") as! String
-          let cost = doc.get("item_cost") as! NSNumber
-          let ratings = doc.get("item_ratings") as! String
-          let image = doc.get("item_image") as! String
-          let details = doc.get("item_details") as! String
-          
-          return Item(id: id, item_name: name, item_cost: cost, item_details: details,
-              item_image: image, item_ratings: ratings)
-          
-          
-        })
+        return Item(id: id, item_name: name, item_cost: cost, item_details: details,
+                    item_image: image, item_ratings: ratings)
         
-        self.filtered = self.items
+        
+      })
+      
+      self.filtered = self.items
+    }
+  }
+  
+  // search or filter
+  func filterData(){
+    withAnimation(.linear){
+      self.filtered = self.items.filter{
+        return $0.item_name.lowercased().contains(self.search.lowercased())
       }
     }
+  }
+  
+  func addToCart(item: Item){
     
-    // search or filter
-    func filterData(){
-      withAnimation(.linear){
-        self.filtered = self.items.filter{
-          return $0.item_name.lowercased().contains(self.search.lowercased())
+    // checking it is added
+    self.items[getIndex(item: item, isCartIndex: false)].isAdded = !item.isAdded
+    
+    //updating filtered array also for search bar result
+    let filterIndex = self.filtered.firstIndex{ (item1) -> Bool in
+      return item.id == item1.id
+    } ?? 0
+    self.filtered[filterIndex].isAdded = !item.isAdded
+    
+    if item.isAdded{
+      //removing from list
+      self.cartItems.remove(at: getIndex(item: item, isCartIndex: true))
+      return
+    }
+    
+    //else adding
+    self.cartItems.append(Cart(item: item, quantity: 1))
+  }
+  
+  func getIndex(item: Item, isCartIndex: Bool) -> Int{
+    
+    let index = self.items.firstIndex{ (item1) -> Bool in
+      
+      return item.id == item1.id
+    } ?? 0
+    
+    let cartIndex = self.cartItems.firstIndex{ (item1) -> Bool in
+      
+      return item.id == item1.id
+    } ?? 0
+    
+    return isCartIndex ? cartIndex : index
+  }
+  
+  func calculateTotalPrice() -> String{
+    var price : Float = 0
+    
+    cartItems.forEach{ (item) in
+      price += Float(item.quantity) * Float(truncating: item.item.item_cost)
+    }
+    
+    return getPrice(value: price)
+  }
+  
+  func getPrice(value: Float) -> String{
+    let format = NumberFormatter()
+    format.numberStyle = .currency
+    
+    return format.string(from: NSNumber(value: value)) ?? ""
+  }
+  
+  // writing order data into firestore
+  func updateOrder(){
+    let db = Firestore.firestore()
+    
+    if ordered{
+      
+      ordered = false
+      db.collection("Users").document(Auth.auth().currentUser!.uid).delete{ (err) in
+        if err != nil{
+          self.ordered = true
         }
       }
+      
+      return
+    }
+    // creating dict of food details
+    var details: [[String:Any]] = []
+    
+    cartItems.forEach{ (cart) in
+      details.append([
+        "item_name": cart.item.item_name,
+        "item_quantity": cart.quantity,
+        "item_cost": cart.item.item_cost
+      ])
     }
     
+    ordered = true
+    
+    db.collection("Users").document(Auth.auth().currentUser!.uid).setData([
+      "ordered_food": details,
+      "total_cost": calculateTotalPrice(),
+      "location": GeoPoint(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+    ]){ (err) in
+      
+      if err != nil{
+        self.ordered = false
+        return
+      }
+      
+      print("Success")
+    }
   }
+}
 
 
